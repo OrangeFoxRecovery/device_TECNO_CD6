67b0523e0f676d2c0718653904f9151efa5dd3e6
Sushrut1101 <75667593+Sushrut1101@users.noreply.github.com>
2021-05-16 23:09:38 +0530

    Update omni_CD6.mk

--- omni_CD6.mk
+++ omni_CD6.mk
@@ -1,6 +1,6 @@
 #
-# Copyright (C) 2019 Potato Open Sauce Project
-#
+# Copyright (C) 2021 The OrangeFox Recovery Project
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -38,7 +38,6 @@ PRODUCT_MANUFACTURER := TECNO
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
-
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=CD6-OP \
