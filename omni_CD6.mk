84a6d8a91024b1b973dfd460869570413c3eed72
Sushrut1101 <75667593+Sushrut1101@users.noreply.github.com>
2021-05-16 23:06:40 +0530

    Update Device Model; Remove Unwanted Stuff; pb --> omni

--- omni_CD6.mk
+++ omni_CD6.mk
@@ -23,25 +23,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/TECNO/CD6/recovery/root,recovery/root) \
     device/TECNO/CD6/prebuilt/dtb:dtb.img
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := CD6
-PRODUCT_NAME := omni_$(PRODUCT_DEVICE)
+PRODUCT_NAME := omni_CD6
 PRODUCT_BRAND := TECNO
-PRODUCT_MODEL := TECNO CD6
-PRODUCT_MANUFACTURER := TECNO MOBILE LIMITED
+PRODUCT_MODEL := Camon 15 Air
+PRODUCT_MANUFACTURER := TECNO
 
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
-	
-# Resolution
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1600
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=CD6-OP \
